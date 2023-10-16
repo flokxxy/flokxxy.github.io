eee73bbe2bc29f8cfc16c4fc0c737504f8f53694
flokxxy <115942207+flokxxy@users.noreply.github.com>
2023-10-16 22:40:04 +0300

    Update main.js

--- lab6/main.js
+++ lab6/main.js
@@ -10,11 +10,26 @@ function calculatePrice() {
   if (serviceType === "type1") {
     price = 10 * quantity;
   } else if (serviceType === "type2") {
-    price = 20 * quantity;
+    price = 15 * quantity;
+    price += option === "option0" ? 0 : 0;
     price += option === "option1" ? 5 : 0;
+    price += option === "option2" ? 10 : 0;
+    price += option === "option3" ? 15 : 0;
   } else if (serviceType === "type3") {
-    price = 30 * quantity;
+    price = 20 * quantity;
     price += property ? 10 : 0;
+  }
+
+
+  //поиграла
+  else if (serviceType === "type4") {
+    price = 25 * quantity;
+    price += property ? 15 : 0;
+    price += option === "option1" ? 5 : 0;
+  } else if (serviceType === "type5") {
+    price = 40 * quantity+quantity*25;
+    price += option === "option2" ? 15 : 0;
+    price += property ? 15 : 0;
   }
 
   // Вывод цены на страницу
