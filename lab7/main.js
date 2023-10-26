86e63f761b3cdf5b49a858361cab61e3dbace939
flokxxy <115942207+flokxxy@users.noreply.github.com>
2023-10-26 17:42:16 +0200

    Update main.js

--- lab7/main.js
+++ lab7/main.js
@@ -1,36 +1,12 @@
+let horisontScroll = document.querySelector('.container');
+let leftBtn = document.getElementById('leftBtn');
+let rightBtn = document.getElementById('rightBtn');
 
-$(document).ready(function() {
-  var slider = $('.gallery-slider');
-  var images = slider.find('img');
-  var pager = $('.gallery-pager');
-  var currentPage = 0;
-
-  // Определение количества страниц
-  var totalPages = Math.ceil(images.length / 3);
-
-  // Генерация пейджера
-  for (var i = 0; i < totalPages; i++) {
-    pager.append('<span></span>');
-  }
-
-  // Установка активной страницы
-  pager.find('span').eq(currentPage).addClass('active');
-
-  // Обработчик клика по стрелке влево
-  $('.arrow-left').click(function() {
-    if (currentPage > 0) {
-      currentPage--;
-      slider.css('transform', 'translateX(' + (-currentPage * 100) + '%)');
-      pager.find('span').removeClass('active').eq(currentPage).addClass('active');
-    }
-  });
-
-  // Обработчик клика по стрелке вправо
-  $('.arrow-right').click(function() {
-    if (currentPage < totalPages - 1) {
-      currentPage++;
-      slider.css('transform', 'translateX(' + (-currentPage * 100) + '%)');
-      pager.find('span').removeClass('active').eq(currentPage).addClass('active');
-    }
-  });
+rightBtn.addEventListener('click', ()=>{
+  horisontScroll.style.scrollBehavior = "smooth";
+  horisontScroll.scrollLeft +=900;
+});
+leftBtn.addEventListener('click', ()=>{
+  horisontScroll.style.scrollBehavior = "smooth";
+  horisontScroll.scrollLeft -= 900;
 });
