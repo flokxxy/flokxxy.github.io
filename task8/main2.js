4c733b2acaf8265cbb085f66d8f439336ef105e6
flokxxy <115942207+flokxxy@users.noreply.github.com>
2023-11-22 19:56:46 +0300

    Update main2.js

--- task8/main2.js
+++ task8/main2.js
@@ -1,31 +1,124 @@
-const button = document.querySelector('button');
-const form = document.querySelector('#blablabla');
-const popup = document.querySelector('.content');
-const closeImage = document.querySelector('.page_close');
-button.addEventListener('click', () => {
-  form.classList.add('open');
-  popup.classList.add('popup_open');
-});
-// Добавляем обработчик события для закрытия формы при клике на изображение
-closeImage.addEventListener('click', () => {
-  form.classList.remove('open');
-  popup.classList.remove('popup_open');
-});
+/* global $ */
 
 
+$(document).ready(function () {
 
-jQuery(function() {
-  $('#mainForm').on('submit', function(e) {
-    e.preventDefault();
-    $.ajax({
-      url: '<Место для ссылки на вашу облачную функцию>',
-      type: 'POST',
-      cache: false,
-      data: $(e.target).serialize()
-    }).done(function() {
-      alert('Done.');
-    }).fail(function() {
-      alert('Something went wrong.');
+  const form = document.getElementById("mainForm");
+  const Name = document.getElementById("name_field");
+  const Email = document.getElementById("email_field");
+  const Phone = document.getElementById("phone_field");
+  const Organization = document.getElementById("company_field");
+  const Message = document.getElementById("desc_field");
+  const Check = document.getElementById("Check1");
+
+  const qform = document.querySelector('#blablabla');
+  const qpopup = document.querySelector('.content');
+  const closeImage = document.querySelector('.page_close');
+
+  const formDataLoad = JSON.parse(localStorage.getItem("formData"));
+  var formDataSave;
+
+  const currentUrl = window.location.href;
+
+  if (currentUrl.includes("#popup__container")) {
+    OpenPopup();
+  }
+
+  // Save form data to localStorage
+  function SaveData() {
+    formDataSave = {
+      consent: Check.checked,
+      email: Email.value,
+      fullName: Name.value,
+      message: Message.value,
+      organization: Organization.value,
+      phone: Phone.value
+    };
+    localStorage.setItem("formData", JSON.stringify(formDataSave));
+  }
+
+
+  // Load form data from localStorage
+  function LoadData() {
+    if (formDataLoad) {
+      Name.value = formDataLoad.fullName;
+      Email.value = formDataLoad.email;
+      Phone.value = formDataLoad.phone;
+      Organization.value = formDataLoad.organization;
+      Message.value = formDataLoad.message;
+      Check.checked = formDataLoad.consent;
+    }
+
+  }
+
+  // Open the popup form
+  function OpenPopup() {
+    // const overlay = document.getElementById("overlay");
+    qform.classList.add('open');
+    qpopup.classList.add('popup_open');
+   // overlay.style.display = "block";
+    history.pushState({ popupOpen: true }, "", "#popup__container");
+  }
+
+
+  // Close the popup form
+  function ClosePopup() {
+    qform.classList.remove('open');
+    qpopup.classList.remove('popup_open');
+    history.back();
+  }
+
+  // Clear form data
+  function ClearForm() {
+    localStorage.clear();
+  }
+
+  // Event listeners
+  document.getElementById("main_button").addEventListener("click", OpenPopup);
+  //document.getElementById("overlay").addEventListener("click", ClosePopup);
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const formData = new FormData(form);
+    fetch("https://formcarry.com/s/CBT-xvtxJq", {
+      body: formData,
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      method: "POST"
+    }).then(function (response) {
+      if (response.ok) {
+        document.getElementById("result").innerHTML =
+          "Данные формы успешно отправлены на сервер";
+        ClearForm();
+      } else {
+        document.getElementById("result").innerHTML =
+          "Ошибка при отправке данных формы на сервер";
+      }
+    }).catch(function (error) {
+      document.getElementById("result").innerHTML = "Произошла ошибка:" + error;
     });
+
+    //form.reset();
   });
+
+  LoadData();
+  window.addEventListener("popstate", function (event) {
+    if (event.state !== null && event.state.popupOpen) {
+      qform.classList.add('open');
+      qpopup.classList.add('popup_open');
+    } else {
+      qform.classList.remove('open');
+      qpopup.classList.remove('popup_open');
+
+    }
+  });
+
+  Name.addEventListener("blur", SaveData);
+  Email.addEventListener("blur", SaveData);
+  Phone.addEventListener("blur", SaveData);
+  Organization.addEventListener("blur", SaveData);
+  Message.addEventListener("blur", SaveData);
+  Check.addEventListener("change", SaveData);
+  closeImage.addEventListener("click",ClosePopup);
 });
